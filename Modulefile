31ea44822ceab2c76399fb164f0bcda7d736aa98
Sander Bilo <sander.bilo@gmail.com>
2013-12-10 19:54:06 +0100

    Initial code

---
+++ Modulefile
@@ -0,0 +1,12 @@
+name    'sbilo-cassandra'
+version '0.1.0'
+source 'git://github.com/sbilo/puppet-cassandra.git'
+author 'Sander Bilo'
+license 'Apache 2.0'
+summary 'Puppet Module For Cassandra Configuration'
+description ''
+project_page 'https://github.com/sbilo/puppet-cassandra'
+
+## Add dependencies, if any:
+# dependency 'username/name', '>= 1.2.0'
+dependency 'puppetlabs-apt', '>=1.4.0'
---
+++ manifests/init.pp
@@ -0,0 +1,19 @@
+class cassandra ($version, $java_version = 'openjdk_1_7_0',) {
+  apt::source { 'cassandra':
+    location   => 'http://www.apache.org/dist/cassandra/debian',
+    release    => $version,
+    key        => '8D77295D',
+    key_server => 'pgp.mit.edu',
+  }
+
+  apt::key { '2B5C1B00':
+    key        => '2B5C1B00',
+    key_server => 'pgp.mit.edu',
+    notify     => Exec['apt_update'],
+  }
+
+  package { 'cassandra':
+    ensure  => installed,
+    require => [Apt::Source['cassandra'], Apt::Key['2B5C1B00']],
+  }
+}
